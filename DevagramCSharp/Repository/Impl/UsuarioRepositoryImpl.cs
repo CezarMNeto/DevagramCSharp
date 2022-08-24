b297159d31999ca6787924a921237342330acd40
Cezar Juventino Martins Neto <103068760+CezarMNeto@users.noreply.github.com>
2022-08-24 11:17:30 -0300

    Atualização Usuario

--- DevagramCSharp/Repository/Impl/UsuarioRepositoryImpl.cs
+++ DevagramCSharp/Repository/Impl/UsuarioRepositoryImpl.cs
@@ -11,6 +11,12 @@ namespace DevagramCSharp.Repository.Impl
         {
             _context = context;        }
 
+        public void AtualizarUsuario(Usuario usuario)
+        {
+            _context.Update(usuario);
+            _context.SaveChanges();
+        }
+
         public Usuario GetUsuarioPorId(int id)
         {
             return _context.Usuarios.FirstOrDefault(u => u.Id == id);
